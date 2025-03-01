05c5e671ab23fa436ead2fb7626399de90134ac4
Kate <katyakychka@gmail.com>
2025-03-01 23:49:40 +0300

    Implemented bidirectional OneToOne mapping between User and Car, added method to find User by Car model and series, and configured Hibernate to update schema. Encountered NullPointerException due to missing Car association.

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,59 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+   public static void main(String[] args) throws SQLException {
+      AnnotationConfigApplicationContext context = 
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+      UserService userService = context.getBean(UserService.class);
+
+      Car car1 = new Car("BMW", 5);
+      Car car2 = new Car("Mercedes", 3);
+      Car car3 = new Car("Audi", 7);
+      Car car4 = new Car("Tesla", 1);
+
+      User user1 = new User("User1", "Lastname1", "user1@mail.ru", car1);
+      User user2 = new User("User2", "Lastname2", "user2@mail.ru", car2);
+      User user3 = new User("User3", "Lastname3", "user3@mail.ru", car3);
+      User user4 = new User("User4", "Lastname4", "user4@mail.ru", car4);
+
+      userService.add(user1);
+      userService.add(user2);
+      userService.add(user3);
+      userService.add(user4);
+
+      userService.addCar(car1);
+      userService.addCar(car2);
+      userService.addCar(car3);
+      userService.addCar(car4);
+
+      List<User> users = userService.listUsers();
+      for (User user : users) {
+         System.out.println("Id = " + user.getId());
+         System.out.println("First Name = " + user.getFirstName());
+         System.out.println("Last Name = " + user.getLastName());
+         System.out.println("Email = " + user.getEmail());
+         System.out.println("Car Model = " + user.getCar().getModel());
+         System.out.println("Car Series = " + user.getCar().getSeries());
+         System.out.println();
+      }
+
+      try {
+         User foundUser = userService.findByCarModelAndSeries("BMW", 5);
+         System.out.println("Found User: " + foundUser.getFirstName() + " " + foundUser.getLastName());
+      } catch (Exception e) {
+         System.out.println("Error: " + e.getMessage());
+      }
+
+      context.close();
+   }
+}
---
+++ src/main/java/hiber/service/UserService.java
@@ -0,0 +1,14 @@
+package hiber.service;
+
+import hiber.model.Car;
+import hiber.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    void add(User user);
+    List<User> listUsers();
+    User findByCarModelAndSeries(String model, int series);
+
+    void addCar(Car car1);
+}
